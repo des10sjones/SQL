c72c215eb02468386474f2e6053122de87898b36
Destin Jones <djones@btcwholesale.com>
2025-01-31 09:59:42 -0600

    Update

---
+++ Beginner/6. Limits & Aliasing.sql
@@ -0,0 +1,14 @@
+-- Limit & Aliasing
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1
+;
+
+-- Aliasing
+select gender, avg(age) AS avg_age
+from employee_demographics
+group by gender
+having avg_age > 40 
+;
